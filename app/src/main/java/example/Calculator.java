2503be066a5bab17b7374b4dddf3365f55c1b7af
mayyasamysiv <mayyasamysiv@student.unimelb.edu.au>
2024-02-28 10:13:21 +1100

    Implemented correct methodology for Calculator

--- app/src/main/java/example/Calculator.java
+++ app/src/main/java/example/Calculator.java
@@ -2,22 +2,22 @@ package example;
 
 public class Calculator {
     public int add(int a, int b) {
-        // TODO: Implement this method
-        return 4;
+
+        return a+b;
     }
 
     public int minus(int a, int b) {
-        // TODO: Implement this method
-        return 10;
+
+        return a - b;
     }
 
     public int multiply(int a, int b) {
-        // TODO: Implement this method
-        return 10;
+
+        return a * b;
     }
 
     public double divide(int a, int b) {
-        // TODO: Implement this method
-        return 5;
+
+        return (double) a/b;
     }
 }
